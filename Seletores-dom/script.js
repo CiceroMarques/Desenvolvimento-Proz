a30f331e4bde73bbd6059fd269521cd3c5c2cc3d
CiceroMarques <72270164+CiceroMarques@users.noreply.github.com>
2024-07-30 17:15:54 -0300

    Add files via upload

---
+++ Seletores-dom/script.js
@@ -0,0 +1,10 @@
+const titulo = document.getElementById("titulo");
+const textos = document.getElementsByClassName("texto-simples");
+
+const segundoTitulo = document.querySelector("div h2")
+const textoPorClasse = document.querySelectorAll(".texto-simples");
+
+console.log(segundoTitulo);
+//console.log(textos);
+console.log(titulo);
+console.log(textoPorClasse)
